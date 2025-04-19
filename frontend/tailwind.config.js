87292981927b85d7fd2dbe3600c2054adddbb3fb
abhishek shah <shahabhishek456@gmail.com>
2025-04-19 12:57:30 +0545

    fixed tailwind issue and run build command

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,6 @@ export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
-      "profile-bg":
-        "url('/src/assets/profile-background-b5vedq7mz8mjvslu.jpg')",
     },
   },
   plugins: [],
