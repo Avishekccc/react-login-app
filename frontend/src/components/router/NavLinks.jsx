df6915aae5108e93f88373f20a0c11b360be50db
abhishek shah <shahabhishek456@gmail.com>
2025-04-23 03:19:49 +0545

    add favicon navlink color and hovereffect

--- frontend/src/components/router/NavLinks.jsx
+++ frontend/src/components/router/NavLinks.jsx
@@ -17,30 +17,59 @@ const NavLinks = () => {
   // console.log(global)
   
   return (
-  
-    <div className="bg-white/50 bg-opacity-100 h-[6vh] flex justify-between items-center px-10 relative">
-      <h1 className="text-lg font-bold">Login System</h1>
+    <div className="bg-white/50 bg-opacity-100 h-[6vh] flex justify-between items-center px-7 relative">
+      <h1 className="text-lg font-bold text-blue-700">Login System</h1>
 
       {/* Desktop Menu */}
       <div className="hidden md:flex space-x-6">
         {global.token ? (
           <>
-            <NavLink to="/admin/my-profile">My Profile</NavLink>
-            <NavLink to="/admin/update-password">Update Password</NavLink>
-            <NavLink to="/admin/read-all-user">Read All User</NavLink>
-            <NavLink to="/admin/logout">Logout</NavLink>
+            <NavLink
+              to="/admin/my-profile"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              My Profile
+            </NavLink>
+            <NavLink
+              to="/admin/update-password"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Update Password
+            </NavLink>
+            <NavLink
+              to="/admin/read-all-user"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Read All User
+            </NavLink>
+            <NavLink
+              to="/admin/logout"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Logout
+            </NavLink>
           </>
         ) : (
           <>
-            <NavLink to="/admin/login">Admin Login</NavLink>
-            <NavLink to="/admin/register">Admin Register</NavLink>
+            <NavLink
+              to="/admin/login"
+              className="text-blue-600 hover:text-blue-700"
+            >
+              Admin Login
+            </NavLink>
+            <NavLink
+              to="/admin/register"
+              className="text-blue-600 hover:text-blue-700"
+            >
+              Admin Register
+            </NavLink>
           </>
         )}
       </div>
 
       {/* Mobile Menu Button */}
       <div
-        className="md:hidden text-3xl cursor-pointer"
+        className="md:hidden text-3xl cursor-pointer text-blue-500 "
         onClick={() => setIsOpen(!isOpen)}
       >
         <i className="bx bx-menu"></i>
@@ -51,31 +80,49 @@ const NavLinks = () => {
         <div className="absolute top-[6vh] left-0 bg-gray-200 w-full shadow-lg py-4 flex flex-col items-center gap-4 md:hidden z-10">
           {global.token ? (
             <>
-               <NavLink to="/admin/my-profile"  onClick={() => setIsOpen(false)}>
+              <NavLink
+                to="/admin/my-profile"
+                onClick={() => setIsOpen(false)}
+                className="text-blue-500"
+              >
                 My Profile
               </NavLink>
               <NavLink
                 to="/admin/update-password"
                 onClick={() => setIsOpen(false)}
+                className="text-blue-500"
               >
                 Update Password
               </NavLink>
               <NavLink
                 to="/admin/read-all-user"
                 onClick={() => setIsOpen(false)}
+                className="text-blue-500"
               >
                 Read All User
               </NavLink>
-              <NavLink to="/admin/logout" onClick={() => setIsOpen(false)}>
+              <NavLink
+                to="/admin/logout"
+                onClick={() => setIsOpen(false)}
+                className="text-blue-500"
+              >
                 Logout
               </NavLink>
             </>
           ) : (
             <>
-              <NavLink to="/admin/login" onClick={() => setIsOpen(false)}>
+              <NavLink
+                to="/admin/login"
+                onClick={() => setIsOpen(false)}
+                className="text-blue-500"
+              >
                 Admin Login
               </NavLink>
-              <NavLink to="/admin/register" onClick={() => setIsOpen(false)}>
+              <NavLink
+                to="/admin/register"
+                onClick={() => setIsOpen(false)}
+                className="text-blue-500"
+              >
                 Admin Register
               </NavLink>
             </>
--- frontend/src/components/webUsersLogin/Dashboard.jsx
+++ frontend/src/components/webUsersLogin/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div className="h-[94vh] bg-gradient-to-br from-[#1B8AF1]  to-white   flex justify-center items-center">
       <div className="text-center space-y-2 p-3">
         <h2 className="text-3xl font-bold text-white">Wellcome To DashBoard</h2>
-        <p className='text-lg font-semibold font-mono'>
+        <p className="text-lg font-semibold text-blue-900">
           This project is built solely to demonstrate my skills. It is a simple
           Login Management System designed for practice and portfolio purposes.
         </p>
